c3472b79d0b7997bdb74f501e6e2e40c8ad75102
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-12-19 17:41:04 -0600

    Changed to render the new activity to add a new task

--- app/src/main/java/com/example/obsinqsob01/dolist/MainActivity.kt
+++ app/src/main/java/com/example/obsinqsob01/dolist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.obsinqsob01.dolist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -9,28 +10,29 @@ import android.widget.BaseAdapter
 import android.widget.Toast
 import com.example.obsinqsob01.dolist.models.Task
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.task_card.view.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var tasks = ArrayList<Task>()
+    private var tasks = ArrayList<Task>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Fill the tasks array with static data
-        (1..10).map {
+        (1..3).map {
             tasks.add(Task(it, "Tarea $it", "Description for $it", Date()))
         }
 
+        list_view_tasks.adapter = TaskAdapter(tasks)
+
         // Click listener to add a new task
-        btn_add_task.setOnClickListener {view ->
-            Snackbar.make(view, R.string.text_add_task, Snackbar.LENGTH_LONG)
-                .setAction("Action", ({
-                    Toast.makeText(applicationContext, R.string.text_add_task, Toast.LENGTH_SHORT)
-                        .show()
-                })).show()
+        btn_add_task.setOnClickListener {
+            val intent = Intent(applicationContext, AddTaskActivity::class.java)
+
+            startActivity(intent)
         }
     }
 
@@ -41,10 +43,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            // somthins
+            var task_layout = layoutInflater.inflate(R.layout.task_card, null)
+            var task = getItem(position)
+
+            task_layout.text_title.text = task.title
+            task_layout.text_description.text = task.description
+            task_layout.text_hour.text = "${task.hour.hours}:${task.hour.minutes}"
+
+            task_layout.setOnClickListener {
+                val intent = Intent(applicationContext, TaskViewActivity::class.java)
+
+                intent.putExtra("task", task)
+
+                startActivity(intent)
+            }
+
+            return task_layout
         }
 
-        override fun getItem(position: Int): Any {
+        override fun getItem(position: Int): Task {
             return this.tasks[position]
         }
 
