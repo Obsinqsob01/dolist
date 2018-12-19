1f81e73eb31b53b386bf22cf2833985fb7c43c5c
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-12-19 17:41:58 -0600

    Other activity, icon and menu

---
+++ app/src/main/java/com/example/obsinqsob01/dolist/AddTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.obsinqsob01.dolist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
