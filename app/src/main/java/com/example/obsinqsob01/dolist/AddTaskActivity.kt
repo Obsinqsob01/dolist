3ca660471dc675f159b2e292b0c4f141bd6f7912
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-12-20 23:09:29 -0600

    Added the timepicker dialog fragment

--- app/src/main/java/com/example/obsinqsob01/dolist/AddTaskActivity.kt
+++ app/src/main/java/com/example/obsinqsob01/dolist/AddTaskActivity.kt
@@ -1,12 +1,21 @@
 package com.example.obsinqsob01.dolist
 
+import android.app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_add_task.*
 
 class AddTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        btn_select_time.setOnClickListener {
+            val timePicker = TimePickerFragment()
+
+            timePicker.show(fragmentManager, "Time Picker")
+        }
     }
 }
