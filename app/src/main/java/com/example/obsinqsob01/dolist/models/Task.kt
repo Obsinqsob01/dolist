88337668d2f3bf2887237afd1c3b0669737e2e74
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-12-20 23:08:51 -0600

    Created user model

--- app/src/main/java/com/example/obsinqsob01/dolist/models/Task.kt
+++ app/src/main/java/com/example/obsinqsob01/dolist/models/Task.kt
@@ -2,5 +2,34 @@ package com.example.obsinqsob01.dolist.models
 
 import java.io.Serializable
 import java.util.*
+import androidx.*
 
-data class Task(val id: Int,var title: String, var description: String, var hour: Date) : Serializable
+@Entity
+data class Task(
+    @PrimaryKey var uid: Int,
+    @ColumnInfo(name = "title") var title: String?,
+    @ColumnInfo(name = "description") var description: String?,
+    @ColumnInfo(name = "hour") var hour: Date?
+) : Serializable
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    fun getAll(): List<Task>
+
+    @Query("SELECT * FROM tasks WHERE uid = :uid")
+    fun findByUID(uid: Int): Task
+
+    @Insert
+    fun insertAll(vararg tasks: Task)
+
+    @Delete
+    fun delete(tasks: Task)
+
+}
+
+@Database(entities = arrayOf(Task::class), version = 1)
+abstract class TaskDatabase() : RoomDatabase() {
+    abstract fun TaskDao() : TaskDao
+}
+
